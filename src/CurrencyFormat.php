26880cc6416e62310e4355f1224965139bf34c8a
aaron-wertheim <aaron.wertheim@realtyads.com>
2023-09-12 09:26:33 -0500

    add more assertions

---
+++ src/CurrencyFormat.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+class CurrencyFormat {
+    public function get_currency_format($amount, $show_plus = true): string {
+        $negative = false;
+
+        if ($amount < 0.0) {
+            $negative = true;
+        } //end if
+
+        $amount = abs($amount);
+
+        $amount_string = number_format($amount);
+
+        if ($negative) {
+            $format = '(%s)';
+        } else if ($show_plus) {
+            $format = '+%s';
+        } else {
+            $format = '%s';
+        } //end if
+
+        return sprintf($format, $amount_string);
+    } //currency_format
+}
