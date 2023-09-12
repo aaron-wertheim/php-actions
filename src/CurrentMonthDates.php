623a0ff15d7c0d9b723dac758d150ec16ddd4539
aaron-wertheim <aaron.wertheim@realtyads.com>
2023-09-12 11:07:28 -0500

    no coverage args

---
+++ src/CurrentMonthDates.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class CurrentMonthDates {
+    function get_current_month_dates(): array {
+        $start_date = date('Y-m-01');
+
+        $end_date = date('Y-m-t');
+
+        return [
+            'start_date' => $start_date,
+            'end_date' => $end_date
+        ];
+    }
+}
---
+++ test/CurrentMonthDatesTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Test;
+require __DIR__ . "/../src/CurrentMonthDates.php";
+
+use App\CurrentMonthDates;
+
+class CurrentMonthDatesTest extends \PHPUnit\Framework\TestCase {
+    public function testCurrencyMonthDates() {
+        $expected = [
+            'start_date' => '2023-09-01',
+            'end_date' => '2023-09-30'
+        ];
+
+        $current_month_dates = new CurrentMonthDates();
+
+        self::assertEquals(
+            $expected,
+            $current_month_dates->get_current_month_dates()
+        );
+
+    }
+
+}
