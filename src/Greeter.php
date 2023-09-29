8a251607716b8e790066d257ace2b5cb1f6dc729
aaron-wertheim <aaron.wertheim@realtyads.com>
2023-09-29 09:57:31 -0500

    unit test in same file

--- src/Greeter.php
+++ src/Greeter.php
@@ -1,12 +1,31 @@
 <?php
-//namespace App;
-class Greeter {
+class Greeter extends PHPUnit\Framework\TestCase
+{
     public function greet(string $name = null):string
     {
         if($name) {
             return "Hello, $name!";
         }
         return "Hello!";
+    }
+
+    public function testGreeterSaysHello()
+    {
+        $this->assertStringContainsString(
+            "Hello",
+            $this->greet()
+        );
+    }
+    public function testGreeterUsesName()
+    {
+        $this->assertStringContainsString(
+            "Hello, Cody",
+            $this->greet("Cody")
+        );
+        $this->assertStringContainsString(
+            "Hello, Sarah",
+            $this->greet("Sarah")
+        );
     }
 
 }
