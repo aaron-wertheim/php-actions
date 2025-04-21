bf6e9be8b6a18e168d8001f0c2dea617b60194d1
aaron-wertheim <aaron.wertheim@realtyads.com>
2025-04-21 10:20:23 -0500

    Pattern Test

---
+++ src/pattern_test.php
@@ -0,0 +1,16 @@
+<?php
+
+// This is a test file to trigger the GitHub Actions scan
+
+// Should trigger: .dev string
+$api_url = "https://my-api.dev/endpoint";
+
+// Should trigger: assignment to $_REQUEST
+$_REQUEST['username'] = $_POST['username'];
+$_REQUEST['password'] = '1234';
+
+// Should NOT trigger (safe usage)
+$user_input = $_REQUEST['input'] ?? null;
+
+// Just a normal line
+echo "Hello, world!";
