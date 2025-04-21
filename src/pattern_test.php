b48b1b7a61f196d23de68cdf73aa0fe1ac7fa2a9
aaron-wertheim <aaron.wertheim@realtyads.com>
2025-04-21 11:49:28 -0500

    Pattern Test

--- src/pattern_test.php
+++ src/pattern_test.php
@@ -9,7 +9,6 @@ $api_url = "https://my-api.dev/endpoint";
 $_REQUEST['username'] = $_POST['username'];
 $_REQUEST['password'] = '1234';
 
-
 // Should NOT trigger (safe usage)
 $user_input = $_REQUEST['input'] ?? null;
 
