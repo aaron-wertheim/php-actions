a8803ddd6acfa041171f40b05362f1739d7c31d7
aaron-wertheim <aaron.wertheim@realtyads.com>
2025-04-28 11:05:25 -0500

    Pattern Test Safe

--- src/pattern_test_2.php
+++ src/pattern_test_2.php
@@ -3,13 +3,13 @@
 // This is a test file to trigger the GitHub Actions scan
 
 // Should trigger: .dev.realtyads.com
-$api_url = "https://my-branch-2.dev.realtyads.com/endpoint2";
+//$api_url = "https://my-branch-2.dev.realtyads.com/endpoint2";
 
 // Should trigger: var/www-dy/
-$path = "/var/www-dy/some-folder-2/file.php";
+//$path = "/var/www-dy/some-folder-2/file.php";
 
 // Should trigger: _dev
-$env = "staging_2_dev.php";
+//$env = "staging_2_dev.php";
 
 // Safe usage (should NOT trigger)
 $other_env = "staging_2";
